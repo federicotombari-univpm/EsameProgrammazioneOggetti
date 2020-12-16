40b955a438be0b85c1e935959be5b47d44844d71
JoshuaSgariglia <S1092411@studenti.univpm.it>
2020-12-16 16:27:50 +0100

    Aggiunti file di base con Spring Initializer

---
+++ ProgettoOpenWeather/src/main/java/it/univpm/ProgettoOpenWeather/ProgettoOpenWeatherApplication.java
@@ -0,0 +1,13 @@
+package it.univpm.ProgettoOpenWeather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoOpenWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoOpenWeatherApplication.class, args);
+	}
+
+}
